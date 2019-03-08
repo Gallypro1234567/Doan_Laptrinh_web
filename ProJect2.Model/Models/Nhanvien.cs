f0991143a34ccacec358f5bd7b3040aeafb96006
TRỌNG TRẦN <tranductrong140198@gmail.com>
2019-03-08 10:59:05 +0700

    commit first

--- ProJect2.Model/Models/Nhanvien.cs
+++ ProJect2.Model/Models/Nhanvien.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -10,19 +11,30 @@ namespace ProJect2.Model.Models
     [Table("Nhanvien")]
     public class Nhanvien
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { set; get; }
 
+        [Required]
+        [MaxLength(200)]
         public string NV_HO { set; get; }
 
+        [Required]
+        [MaxLength(100)]
         public string NV_TEN { set; get; }
 
+        [MaxLength(100)]
         public string NV_GIOITINH { set; get; }
 
         public DateTime NV_NGAYSINH { set; get; }
 
+        [MaxLength(500)]
         public string NV_NOISINH { set; get; }
 
+        [MaxLength(100)]
         public string SDT { set; get; }
 
+        [MaxLength(100)]
+        public string CMNN { set; get; }
     }
 }
