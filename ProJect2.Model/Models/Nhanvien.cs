79d2bde8e61d2cb6fa494bca943aa9b2c8604e1a
TRỌNG TRẦN <tranductrong140198@gmail.com>
2019-03-08 10:51:41 +0700

    Add project files.

---
+++ ProJect2.Model/Models/Nhanvien.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProJect2.Model.Models
+{
+    [Table("Nhanvien")]
+    public class Nhanvien
+    {
+        public int ID { set; get; }
+
+        public string NV_HO { set; get; }
+
+        public string NV_TEN { set; get; }
+
+        public string NV_GIOITINH { set; get; }
+
+        public DateTime NV_NGAYSINH { set; get; }
+
+        public string NV_NOISINH { set; get; }
+
+        public string SDT { set; get; }
+
+    }
+}
